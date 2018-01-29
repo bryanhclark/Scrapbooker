a0432bdaea8cd4b43a8478cd85e895fd5ee0a857
bryanhclark <bryanhclark@gmail.com>
2018-01-29 13:45:38 -0500

    reduxeFirebase - removed react-redux-firebase and completed firebase image upload and component rerender

--- client/store/firebase.js
+++ client/store/firebase.js
@@ -1,71 +1,34 @@
 import axios from 'axios'
 import history from '../history'
 import * as firebase from 'firebase'
+import { config } from '../../secrets'
+import "babel-polyfill"
 
 
 // //ACTION TYPEs
 
 const UPLOAD_TO_FIREBASE = 'UPLOAD_TO_FIREBASE'
-var config = {
-  apiKey: "AIzaSyCgYLyDM38JZEvWOn19tmmrcXM9FtbUwY0",
-  authDomain: "take3-44012.firebaseapp.com",
-  databaseURL: "https://take3-44012.firebaseio.com",
-  projectId: "take3-44012",
-  storageBucket: "take3-44012.appspot.com",
-  messagingSenderId: "807072044786"
-};
-firebase.initializeApp(config)
-
 
 // //ACTION CREATORS
 
-const getPictures = (pictures) => { type: UPLOAD_TO_FIREBASE, pictures }
+const getPictures = (pictures) => {
+    return { type: UPLOAD_TO_FIREBASE, pictures }
+}
 
 
 // //THUNKS
-
-export const uploadImageToFireBaseThunk = (image, eventId) => {
-    eventId = eventId || 1
-    const name = image.name
-    const imageRef = firebase.storage().ref(`images/${eventId}`).child(name).put(image)
-
-    imageRef.on('state_changed', function(snapshot) {
-      let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '%done')
-        switch (snapshot.state) {
-            case firebase.storage.TaskState.PAUSED:
-                console.log('Upload is paused');
-                break;
-            case firebase.storage.TaskState.RUNNING:
-                console.log('Upload is running');
-                break
-        }
-    }, function(err) {
-      console.log(err.code)
-    }, function() {
-      console.log(imageRef.snapshot)
-    })
-    return dispatch => {
-      console.log("SUCCESS")
-    // imageRef.on(firebase.storage.TaskEvent.STATE_CHANGED, (snapshot) => {
-    //     let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    //     console.log('Upload is ' + progress + '%done')
-    //     switch (snapshot.state) {
-    //         case firebase.storage.TaskState.PAUSED:
-    //             console.log('Upload is paused');
-    //             break;
-    //         case firebase.storage.TaskState.RUNNING:
-    //             console.log('Upload is running');
-    //             break
-    //     }
-
-    // }, () => {
-    //     let downloadURL = imageToUpload.snapshot.downloadURL
-    //     console.log(downloadURL)
-    //     dispatch(getPictures(downloadURL))
-    // })
+//async await 
+export const uploadImageToFireBaseThunk = (image) => {
+    return (dispatch) => {
+        axios.post('/api/content', { imageURL: image })
+            .then((response) => {
+                console.log(response.data.downloadURL)
+                dispatch(getPictures(response.data.downloadURL))
+            })
     }
 }
+
+
 
 
 // //REDUCER
@@ -73,7 +36,7 @@ export const uploadImageToFireBaseThunk = (image, eventId) => {
 export default (state = [], action) => {
     switch (action.type) {
         case UPLOAD_TO_FIREBASE:
-            return action.pictures
+            return [...state, action.pictures]
         default:
             return state
     }
--- secrets.js
+++ secrets.js
@@ -1,1 +1,17 @@
+const firebase = require('firebase')
 
+
+const config = {
+    apiKey: "AIzaSyCgYLyDM38JZEvWOn19tmmrcXM9FtbUwY0",
+    authDomain: "take3-44012.firebaseapp.com",
+    databaseURL: "https://take3-44012.firebaseio.com",
+    projectId: "take3-44012",
+    storageBucket: "take3-44012.appspot.com",
+    messagingSenderId: "807072044786"
+};
+
+
+const firebaseInitialize = firebase.initializeApp(config)
+
+
+module.exports = { config, firebaseInitialize };

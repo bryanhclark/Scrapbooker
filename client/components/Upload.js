35f3fafdcd1687f126bd9066fd145deafc1182e1
Tim LaTorre <timlatorre@gmail.com>
2018-01-30 10:44:51 -0500

    Upload: some work on resize

--- client/components/Upload.js
+++ client/components/Upload.js
@@ -4,7 +4,6 @@ import { withRouter, Link, NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { uploadImageToFireBaseThunk } from '../store/firebase'
 
-
 class Upload extends Component {
     constructor(props) {
         super(props)
@@ -36,9 +35,48 @@ const mapState = (state) => {
 const mapDispatch = (dispatch) => {
     return {
         handleUpload(e) {
-            let img = e.target.files[0]
-            dispatch(uploadImageToFireBaseThunk(img));
-        }
+						let img = e.target.files[0]
+
+						// Find dimensions
+						let reader = new FileReader();
+						reader.readAsDataURL(img);
+						reader.onload = function (e) {
+							let image = new Image();
+							image.src = e.target.result;
+							image.onload = function () {
+								let height = this.height;
+								console.log("height is", height)
+								let width = this.width;
+								console.log("width is", width)
+							};
+
+					}
+
+						// console.log("img.width is", img.width)
+						// const MAX_WIDTH = 1200;
+						// const MAX_HEIGHT = 1200;
+						// let width = img.width;
+						// let height = img.height;
+						// let canvas
+						
+						// if (width > height) {
+						// 	if (width > MAX_WIDTH) {
+						// 		height *= MAX_WIDTH / width;
+						// 		width = MAX_WIDTH;
+						// 	}
+						// } else {
+						// 	if (height > MAX_HEIGHT) {
+						// 		width *= MAX_HEIGHT / height;
+						// 		height = MAX_HEIGHT;
+						// 	}
+						// }
+						// canvas.width = width;
+						// canvas.height = height;
+						// var ctx = canvas.getContext("2d");
+						// ctx.drawImage(img, 0, 0, width, height);
+
+            //dispatch(uploadImageToFireBaseThunk(img));
+				}
     }
 }
 

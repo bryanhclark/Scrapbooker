f09e95bca42eedfa2ee66f55798cfd74f409525e
James DeLay <jdelay.jr@gmail.com>
2018-01-31 11:18:07 -0500

    feat: SOCKETS BABY

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,6 +1,12 @@
+// Server
 module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
+
+    socket.on('image_upload', (image) => {
+      console.log("HERE => ", image)
+      socket.broadcast.emit('got_it', {obj: "it's"});
+    })
 
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)

d26f947e9dc25bd7df472521639de0e729534922
James DeLay <jdelay.jr@gmail.com>
2018-02-02 12:00:49 -0500

    Twilio working

--- server/api/twilio.js
+++ server/api/twilio.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const {TwilioConfig, TWILLIONUMBER} = require('../../secrets')
-const {Event} = require('../db/models')
+const {Event, Participants} = require('../db/models')
 const Twilio = require('twilio');
 module.exports = router;
 
@@ -8,23 +8,22 @@ module.exports = router;
 const messageSender = new Twilio(TwilioConfig.accountSid, TwilioConfig.authToken);
 
 //For local testing paste IP here
-const IP = '000.00.00.00'
+const IP = ``;
 
 router.post('/', (req, res, next) => {
-
-  let eventId = req.body.eventId
-
-  Event.findById({eventId, include: [{all: true}]})
-  .then(event => event.getParticipants())
+  Participants.findAll({
+    where: {eventId: req.body.eventId},
+    include: [{all: true}]
+  })
   .then(participants => {
-    //Check the syntax of the incoming phone number
+    // console.log('participants', participants)
+    // Check the syntax of the incoming phone number
     // Must be: "+1__________"
     participants.map(participant => {
+
       return messageSender.messages.create({
-        //Option to add a message to Twilio message use this => ${req.body.message}
-        body: `Event Link: ${IP}:8080/events/${eventId}`,
-        //AFTER TEST CHANGE THIS TO participant.phone
-        to: "+15164580715",
+        body: `http://${IP}:8080/events/${req.body.eventId}`,
+        to: `+1${participant.contact.phone}`,
         from: TWILLIONUMBER
       })
       .then((messageSent) => {

a588c92c9674b5609e01ca2a5ab34ee621f57fec
James DeLay <jdelay.jr@gmail.com>
2018-02-02 16:22:45 -0500

    Twilio Message Working

--- client/store/twilio.js
+++ client/store/twilio.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 
-export function broadcastTextMessage (eventId) {
-  console.log('in broadvast twilio', eventId)
- return axios.post('/api/twilio/', eventId)
+export function broadcastTextMessage (event) {
+  console.log(event)
+ return axios.post('/api/twilio/', event)
     .then(res => console.log("Message sent", res.data))
 }

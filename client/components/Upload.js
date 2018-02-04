f95619eeb3cbd2c0a92434f74c8fbc33d797081b
Tim LaTorre <timlatorre@gmail.com>
2018-02-04 00:54:45 -0500

    resize added

--- client/components/Upload.js
+++ client/components/Upload.js
@@ -36,7 +36,7 @@ class Upload extends Component {
 						(e) => {
 							e.preventDefault()
 							this.fileInput = e.target.files[0];
-							console.log("this.fileInput", this.fileInput)
+							//console.log("this.fileInput", this.fileInput)
 							this.setState({ img: e.target.files[0] })
 						}} />
 					<p>{this.state.img.name}</p>
@@ -58,17 +58,23 @@ const mapState = (state) => {
 const mapDispatch = (dispatch) => {
 	return {
 		handleImgUpload(image, eventId) {
-			firebaseUpload(image)
-				.then(response => {
-					console.log(response)
-					return imageEXIFPacker(image, response, eventId, (error, imageObj) => {
-						if (error) console.error(error)
-						else {
-							dispatch(postContent(imageObj))
-							uploadImageSocket(imageObj)
-						}
-					})
+			console.log("image at start", image)
+			resizeImage({
+				file: image,
+				maxSize: 1200
+			})
+			.then(resizedImg => {
+				resizedImg.name = eventId + "--" + image.name
+				return firebaseUpload(resizedImg)
+			})
+			.then(firebaseURL => {
+				imageEXIFPacker(image, firebaseURL, eventId, (error, imageObj) => {
+					if (error) console.log(error)
+					else {
+						console.log("imageObj", imageObj)
+					}
 				})
+			})
 		},
 		loadSingleEvent(eventId) {
 			dispatch(fetchSingleEvent(eventId))
@@ -77,7 +83,7 @@ const mapDispatch = (dispatch) => {
 }
 
 function imageEXIFPacker(image, url, eventId, cb) {
-	let imgObj = {}
+	const imgObj = {}
 	EXIF.getData(image, function () {
 		imgObj.src = url
 		imgObj.width = Number(EXIF.getTag(this, "PixelXDimension"))
@@ -87,12 +93,7 @@ function imageEXIFPacker(image, url, eventId, cb) {
 		imgObj.eventId = eventId
 		cb(null, imgObj)
 	})
-
 }
-
-const uploadContainer = connect(mapState, mapDispatch)(Upload)
-export default uploadContainer
-
 
 const firebaseUpload = (image) => {
 	const downloadURL = firebase.storage().ref(`images`).child(image.name).put(image)
@@ -101,3 +102,60 @@ const firebaseUpload = (image) => {
 		})
 	return downloadURL
 }
+
+const resizeImage = function (settings) {
+	const file = settings.file
+	const maxSize = settings.maxSize
+	const reader = new FileReader()
+	const image = new Image()
+	const canvas = document.createElement('canvas')
+
+	const dataURItoBlob = function (dataURI) {
+		const bytes = dataURI.split(',')[0].indexOf('base64') >= 0 ?
+			atob(dataURI.split(',')[1]) :
+			unescape(dataURI.split(',')[1]);
+		const mime = dataURI.split(',')[0].split(':')[1].split(';')[0];
+		const max = bytes.length
+		const ia = new Uint8Array(max)
+		for (var i = 0; i < max; i++)
+			ia[i] = bytes.charCodeAt(i)
+		return new Blob([ia], { type: mime })
+	};
+
+	const resize = function () {
+		let width = image.width;
+		let height = image.height;
+		if (width > height) {
+			if (width > maxSize) {
+				height *= maxSize / width;
+				width = maxSize;
+			}
+		} else {
+			if (height > maxSize) {
+				width *= maxSize / height;
+				height = maxSize;
+			}
+		}
+		canvas.width = width;
+		canvas.height = height;
+		canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+		var dataUrl = canvas.toDataURL('image/jpeg');
+		return dataURItoBlob(dataUrl);
+	};
+
+	return new Promise(function (ok, no) {
+		if (!file.type.match(/image.*/)) {
+			no(new Error("Not an image"));
+			return;
+		}
+		reader.onload = function (readerEvent) {
+			image.onload = function () { return ok(resize()); };
+			image.src = readerEvent.target.result;
+		};
+		reader.readAsDataURL(file);
+	});
+};
+
+
+const uploadContainer = connect(mapState, mapDispatch)(Upload)
+export default uploadContainer

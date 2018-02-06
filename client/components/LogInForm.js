5c987c391d68c0c125299d0eb38b00b3dfd70e45
Tim LaTorre <timlatorre@gmail.com>
2018-02-06 14:37:57 -0500

    design: login form cleanup

--- client/components/LogInForm.js
+++ client/components/LogInForm.js
@@ -29,19 +29,16 @@ class LoginForm extends Component {
   render() {
     return (
       <div className='login-Form-Container'>
-        <h4>Login:</h4>
+        <h4 className='modal_header'>Login:</h4>
         <form onSubmit={this.handleSubmit}>
-          <div className='login-Email--Button-Div'>
-            <label>
-              Email:
-                <input type='text' value={this.state.email} name='email' onChange={this.handleChange} />
-            </label>
+          <div className='form_row'>
+            <label>Email:</label>
+            <input type='text' value={this.state.email} name='email' onChange={this.handleChange} />
           </div>
-          <div className='login-Password-Button-Div'>
-            <label>
-              Password:
-                <input type='text' value={this.state.password} name='password' onChange={this.handleChange} />
-            </label>
+          <div className='form_row'>
+            <label>Password:</label>
+            <input type='text' value={this.state.password} name='password' onChange={this.handleChange} />
+            
           </div>
           <div className='btn_area'>
             <input className="btn" type='submit' value='submit' />
